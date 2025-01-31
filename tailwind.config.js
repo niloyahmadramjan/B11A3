6fc31c40a394c9afd4782589c5b9e5bcd35480b9
niloyahmadramjan <niloyahmadramjan@gmail.com>
2025-02-01 01:42:05 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#179800",
+        secondary: "#EFEBE3",
+        textColor: "#111111",
+      },
+    },
+  },
+  plugins: [],
+};
